6ca98e19d118713446b20153fbb5f8a56d370313
Dan <danilobomfim.si@gmail.com>
2021-11-17 18:27:08 -0300

    add

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
-import BotaoConclui from "./componentes/concluiTarefa.js";
-import BotaoDeleta from "./componentes/deletaTarefa.js";
+import BotaoConclui from './componentes/concluiTarefa.js'
+import BotaoDeleta from './componentes/deletaTarefa.js'
+ 
 
-const criarTarefa = (evento) => {
-  evento.preventDefault();
+const handleNovoItem = (evento) => {
+    evento.preventDefault()
 
-  const lista = document.querySelector("[data-list]");
-  const input = document.querySelector("[data-form-input]");
-  const valor = input.value;
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
 
-  const calendario = document.querySelector("[data-form-date]");
-  const data = moment(calendario.value);
-  console.log(data.format("DD/MM/YYYY"));
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
 
-  const tarefa = document.createElement("li");
-  tarefa.classList.add("task");
-  const conteudo = `<p class="content">${valor}</p>`;
+    const dataFormatada = data.format('DD/MM/YYYY')
 
-  tarefa.innerHTML = conteudo;
+    const dados = { 
+        valor,
+        dataFormatada
+    }
 
-  tarefa.appendChild(BotaoConclui());
-  tarefa.appendChild(BotaoDeleta());
-  lista.appendChild(tarefa);
-  input.value = " ";
-};
+    const criaTarefa = criarTarefa(dados)
 
-const novaTarefa = document.querySelector("[data-form-button]");
+    lista.appendChild(criaTarefa)
 
-novaTarefa.addEventListener("click", criarTarefa);
+    sessionStorage.setItem('tarefas',  dados )
+    input.value = " "
+}
+
+const criarTarefa = ({ valor, dataFormatada }) => {
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+   
+    return tarefa
+
+}
+
+const novaTarefa = document.querySelector('[data-form-button]')
+
+novaTarefa.addEventListener('click', handleNovoItem)
